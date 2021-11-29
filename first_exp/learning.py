aba0794d6df405de3d68b49e50e9e2ad293314cd
sonntaghimself <sami.sonntag@gmail.com>
2021-11-29 14:16:07 +0100

    first little project completed

--- first_exp/learning.py
+++ first_exp/learning.py
@@ -4,12 +4,37 @@
 import pandas as pd
 import numpy as np
 import matplotlib
-
-# import scikitlearn
+from sklearn.tree import DecisionTreeClassifier as dtc
 
 ###############################################################################
 #                               importing data                                #
 ###############################################################################
-df = pd.read_csv("~/Desktop/machine_learning/vgsales.csv")
+df = pd.read_csv("~/Desktop/machine_learning/first_exp/data/vgsales.csv")
+
+################
+#  first look  #
+################
 df.shape
 df.describe()
+df.values
+
+df.hist()
+
+###############################################################################
+#                                 music task                                  #
+###############################################################################
+music_data = pd.read_csv("~/Desktop/machine_learning/first_exp/data/music.csv")
+
+####################################################################
+#  splitting the data set into an input(X), and an output(Y) one   #
+####################################################################
+X = music_data.drop(columns=["genre"])
+Y = music_data["genre"]
+
+model = dtc()
+model.fit(X, Y)
+predictions = model.predict([[21, 1], [22, 0]])
+predictions
+# Out[28]: array(['HipHop', 'Dance'], dtype=object)
+# says that a 21 year old male would prefer HipHop, whereas the 22 year old
+# female would prefer to listen to dance music
